b5cc9888e70275fa86fbb5fe97151b10ca553dbf
08gsxr <32687276+08gsxr@users.noreply.github.com>
2018-01-13 15:37:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,4 +54,10 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', msg => {
+  if (msg.content === prefix + 'bri') {
+    msg.reply('Perk Up');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
